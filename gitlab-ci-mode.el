99214277a0ea0f20472631e05ba8302997d5d364
Joe Wreschnig <joe.wreschnig@gmail.com>
2018-11-27 19:41:15 +0100

    Bump package version for new variables

--- gitlab-ci-mode.el
+++ gitlab-ci-mode.el
@@ -5,7 +5,7 @@
 ;; Author: Joe Wreschnig
 ;; Keywords: tools, vc
 ;; Package-Requires: ((emacs "25") (yaml-mode "0.0.12"))
-;; Package-Version: 20180605.1
+;; Package-Version: 20181127.2
 ;; URL: https://gitlab.com/joewreschnig/gitlab-ci-mode/
 ;;
 ;; This program is free software; you can redistribute it and/or modify
